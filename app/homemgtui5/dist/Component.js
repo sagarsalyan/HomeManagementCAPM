756551147648ef92c2538c737b16deb9b8baf42a
Sagar Salyan <sagarslyn@gmail.com>
2026-01-22 11:54:38 +0000

    Added UI in MTA

--- app/homemgtui5/dist/Component.js
+++
@@ -1,2 +0,0 @@
-sap.ui.define(["sap/ui/core/UIComponent","app/homemgtui5/model/models"],(e,t)=>{"use strict";return e.extend("app.homemgtui5.Component",{metadata:{manifest:"json",interfaces:["sap.ui.core.IAsyncContentCreation"]},init(){e.prototype.init.apply(this,arguments);this.setModel(t.createDeviceModel(),"device");this.getRouter().initialize()}})});
-//# sourceMappingURL=Component.js.map
--- app/services.cds
+++ app/services.cds
@@ -1,2 +1,2 @@
 
-using from './homemgtui5/annotations';
+using from './project1/annotations';
